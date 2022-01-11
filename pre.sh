ddb4cbdf781786a6e4c8a0e9a189c9afd1b4993e
karmab <karimboumedhel@gmail.com>
2022-01-11 16:35:57 +0100

    update procedure

--- pre.sh
+++ pre.sh
@@ -1,12 +1,7 @@
-echo """[kubernetes]
-name=Kubernetes
-baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
-enabled=1
-gpgcheck=0
-repo_gpgcheck=0""" >/etc/yum.repos.d/kubernetes.repo
-yum -y install kubectl
-KUBECTL_VERSION=$(dnf --showduplicates list kubectl  | grep kubectl | tail -1 | awk '{print $2}' | xargs)
-CRIO_VERSION=$(echo $KUBECTL_VERSION | cut -d. -f1,2)
+curl -o oc.tar.gz https://mirror.openshift.com/pub/openshift-v4/clients/oc/latest/linux/oc.tar.gz
+tar -xzvf oc.tar.gz
+install -t /usr/local/bin {kubectl,oc}
+CRIO_VERSION=1.$(kubectl version -o yaml | grep minor | cut -d: -f2 | sed 's/"//g' | xargs)
 OS="CentOS_8"
 curl -L -o /etc/yum.repos.d/devel:kubic:libcontainers:stable.repo https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/devel:kubic:libcontainers:stable.repo
 curl -L -o /etc/yum.repos.d/devel:kubic:libcontainers:stable:cri-o:$CRIO_VERSION.repo https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable:cri-o:$CRIO_VERSION/$OS/devel:kubic:libcontainers:stable:cri-o:$CRIO_VERSION.repo
