9eccc248ee8376786dc5f26224f614caf0987b6d
karmab <karimboumedhel@gmail.com>
2022-01-28 21:56:00 +0100

    proper auth.json

--- 01_clients.sh
+++ 01_clients.sh
@@ -2,4 +2,4 @@ curl -o oc.tar.gz https://mirror.openshift.com/pub/openshift-v4/clients/oc/lates
 tar -xzvf oc.tar.gz
 install -t /usr/bin {kubectl,oc}
 dnf -y install podman
-test -f /root/openshift_pull.json && podman login registry.redhat.io --authfile /root/openshift_pull.json
+test -f /root/auth.json && podman login registry.redhat.io --authfile /root/auth.json
