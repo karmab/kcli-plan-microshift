504b57c19e6ead105d25ebda0afe860de5d71823
karmab <karimboumedhel@gmail.com>
2022-01-28 19:33:30 +0100

    allow to use nip

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,8 @@
+{% if nip %}
+IP=`ip a l  eth0 | grep 'inet ' | cut -d' ' -f6 | awk -F'/' '{ print $1}'`
+echo $IP.nip.io > /etc/hostname
+hostnamectl set-hostname $IP.nip.io
+{% endif %}
 /root/01_clients.sh
 /root/02_crio.sh
 /root/03_microshift.sh
